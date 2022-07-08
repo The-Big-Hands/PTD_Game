27880ab2a08e3a2c58d8a3d32cf3840489d14075
LeeJinGue <hippo9851@gmail.com>
2022-07-08 20:53:48 +0900

    feat: Add Game Class
    게임을 관리할 게임 클래스를 만들었습니다.

--- function.js
+++ function.js
@@ -3,3 +3,32 @@ function rollDice(){
   return Math.floor(Math.random() * 6)+1
 }
 console.log(rollDice())
+
+// 게임: Player, Turn, Dice, Target Score 정보를 갖고있는 객체
+// turn이 0이면 Player1의 turn
+class Game{
+  constructor(targetScore){
+    this.setTargetScore(targetScore)
+    this.turn = 0
+  }
+  // Target Score를 업데이트합니다.
+  setTargetScore(targetScore){
+    this.targetScore = targetScore
+  }
+  // Player리스트를 세팅합니다.
+  setPlayers(playerList){
+    // 넘겨받은 playerList를 this.playerList에 세팅합니다.
+    for(let i=0; i<playerList.length; i++){
+      this.playerList[i] = playerList[i]
+    }
+    // this.playerList = playerList
+  }
+  // 다음 턴으로 넘깁니다.
+  nextTurn(){
+    if(this.turn === 1){
+      this.turn === 0
+    }else{
+      this.turn === 1
+    }
+  }
+}
